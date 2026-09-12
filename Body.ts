966d5868d5ed9e334678d65b5e7b7fc8ec5ce1fd
Nikko <neilnikkomarianno.lut-24@cpu.edu.ph>
2026-09-12 22:34:42 +0800

    Added Sample Printer List

--- Body.ts
+++ Body.ts
@@ -4,4 +4,9 @@ import {print} from "./print_function"
 
 
 print("Hello World");
-HELLO
+
+
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumberList = numberList.filter((number) => number % 2 === 0);
+print(evenNumberList);
