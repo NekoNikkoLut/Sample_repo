de44cdab5deb8343af2b9a0cb3c32d50282d3913
Nikko <neilnikkomarianno.lut-24@cpu.edu.ph>
2026-09-14 22:27:16 +0800

    Added some sorts of stuff For statpad

--- Body.ts
+++ Body.ts
@@ -10,3 +10,19 @@ const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const evenNumberList = numberList.filter((number) => number % 2 === 0);
 print(evenNumberList);
+
+
+const oddNumberList = numberList.filter((number) => number % 2 !== 0);
+print(oddNumberList);
+
+const sumOfEvenNumberList = evenNumberList.reduce((sum, number) => sum + number, 0);
+print(sumOfEvenNumberList);
+
+const sumOfOddNumberList = oddNumberList.reduce((sum, number) => sum + number, 0);
+print(sumOfOddNumberList);
+
+const sumOfAllNumberList = numberList.reduce((sum, number) => sum + number, 0);
+print(sumOfAllNumberList);
+
+const averageOfAllNumberList = sumOfAllNumberList / numberList.length;
+print(averageOfAllNumberList);
